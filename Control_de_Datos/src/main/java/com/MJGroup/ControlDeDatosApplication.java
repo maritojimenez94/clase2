e5c6d65185076fafc015b1abeab8c0794277c592
maritojimenez94 <43118953+maritojimenez94@users.noreply.github.com>
2018-09-16 13:34:04 -0600

    Initial commit

---
+++ Control_de_Datos/src/main/java/com/MJGroup/ControlDeDatosApplication.java
@@ -0,0 +1,12 @@
+package com.MJGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeDatosApplication.class, args);
+	}
+}
---
+++ Control_de_Datos/src/main/java/com/MJGroup/models/entities/Pacientes.java
@@ -0,0 +1,10 @@
+package com.MJGroup.models.entities;
+
+public class Pacientes {
+	@Entity
+	@Table(name="Pacientes")
+	@Id: Nombre
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator
+	= " {Pacientes_Seq} ")
+	
+}
